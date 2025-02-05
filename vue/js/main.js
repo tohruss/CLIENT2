5200c5985ed689741dd4bceee006b00ac6e7c967
tohruss <taraskind6@gmail.com>
2025-02-05 12:25:16 +0700

    рабочая карточка

--- vue/js/main.js
+++ vue/js/main.js
@@ -13,11 +13,12 @@ Vue.component('task-card', {
         <input 
             type="text" 
             v-model="newTask" 
-            placeholder="Enter a task" 
+            placeholder="Введите задачи" 
             @keyup.enter="addTask"
         />
-        <button @click="addTask" :disabled="card.tasks.length >= 5">Add Task</button>
-        <p v-if="card.tasks.length >= 5" style="color: red;">You can only add up to 5 tasks.</p>
+        <button @click="addTask" :disabled="card.tasks.length >= 5">Добавить</button>
+        <p v-if="card.tasks.length < 3" style="color: red;">Вам нужно добавить минимум 3 задачи!</p>
+        <p v-if="card.tasks.length >= 5" style="color: red;">Вы можете добавить не более 5 задач!</p>
     </div>
     `,
     data() {
@@ -27,10 +28,24 @@ Vue.component('task-card', {
     },
     methods: {
         addTask() {
-            if (this.newTask.trim() && this.card.tasks.length < 5) {
+            if (this.card.tasks.length < 5 ) {
                 this.card.tasks.push({ text: this.newTask, completed: false });
                 this.newTask = '';
+                this.saveTasks();
             }
+        },
+        saveTasks(){
+            const cards = JSON.parse(localStorage.getItem('cards')) || [];
+            const cardIndex = this.$parent.cards.indexOf(this.card);
+            cards[cardIndex] = this.card;
+            localStorage.setItem('cards', JSON.stringify(cards));
+        },
+    },
+    mounted() {
+        const cards = JSON.parse(localStorage.getItem('cards')) || [];
+        const cardIndex = this.$parent.cards.indexOf(this.card);
+        if (cards[cardIndex]) {
+            this.card.tasks = cards[cardIndex].tasks;
         }
     }
 });
@@ -45,10 +60,25 @@ new Vue({
     },
     methods: {
         addCard() {
-            if (this.newCardTitle.trim()) {
+            if (this.newCardTitle) {
                 this.cards.push({ title: this.newCardTitle, tasks: [] });
                 this.newCardTitle = ''; // Очистка поля ввода заголовка
+                this.saveCards();
             }
+        },
+        saveCards() {
+            localStorage.setItem('cards', JSON.stringify(this.cards));
+        },
+        deleteAllCards() {
+            this.cards = [];
+            localStorage.removeItem('cards');
+        }
+    },
+    mounted() {
+        // Загружаем карточки при монтировании
+        const savedCards = JSON.parse(localStorage.getItem('cards'));
+        if (savedCards) {
+            this.cards = savedCards;
         }
     },
     template: `
@@ -56,10 +86,10 @@ new Vue({
         <input 
             type="text" 
             v-model="newCardTitle" 
-            placeholder="Enter card title" 
+            placeholder="Введите название карточки" 
         />
-        <button @click="addCard">Add Task Card</button>
-        
+        <button @click="addCard">Добавить карточку</button>
+        <button @click="deleteAllCards">Удалить все карточки</button>
         <div v-for="(card, index) in cards" :key="index">
             <task-card :card="card"></task-card>
         </div>
